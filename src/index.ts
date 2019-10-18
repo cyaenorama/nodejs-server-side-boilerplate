e1499483f9c501001237eb3ea242ab4d32945f9d
Sayan Das <sayan.das2486@gmail.com>
2019-10-18 20:08:31 +0530

    refactor(app.ts, index.ts): index.ts added

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,12 @@
 import dotenv from 'dotenv';
-import express from 'express';
+import http from 'http';
+
+import app from './app';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 8080;
 
-const app = express();
-
-app.get('/', (_, res) => {
-  res.send(
-    '<h2>NodeJS Server Side Starter Boilerplate</h2><p>Basic NodeJS Server Side Boilerplate With Typescript Support.</p>'
-  );
-});
-
-app.listen(PORT, () => {
+http.createServer(app).listen(PORT, () => {
   console.log(`server is listening at port: ${PORT}`);
 });
