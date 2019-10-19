682512f1ddbb63bde2730e32a6e978238ba0d817
Sayan Das <sayan.das2486@gmail.com>
2019-10-19 18:15:10 +0530

    feat(index.ts): https support added

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,46 @@
 import dotenv from 'dotenv';
+import fs from 'fs-extra';
 import http from 'http';
+import https from 'https';
 
 import app from './app';
 
 dotenv.config();
 
-const PORT = process.env.PORT || 8080;
+const { HOSTNAME, PORT } = process.env;
 
-http.createServer(app).listen(PORT, () => {
-  console.log(`server is listening at port: ${PORT}`);
-});
+const configs = {
+  // You may need sudo to run on port 443
+  // production: { ssl: true, port: 443, hostname: HOSTNAME },
+  production: { ssl: false, port: PORT, hostname: HOSTNAME },
+  development: { ssl: false, port: PORT, hostname: HOSTNAME }
+};
+
+const environment = process.env.NODE_ENV || 'production';
+const { ssl, port, hostname } = configs[environment];
+
+(() => {
+  try {
+    let server: http.Server;
+
+    if (ssl) {
+      server = https.createServer(
+        {
+          key: fs.readFileSync(`.ssl/server.key`),
+          cert: fs.readFileSync(`.ssl/server.crt`)
+        },
+        app
+      );
+    } else {
+      server = http.createServer(app);
+    }
+
+    server.listen({ port }, () => {
+      console.log(
+        `server running at: http${ssl ? 's' : ''}://${hostname}:${port}/`
+      );
+    });
+  } catch (err) {
+    console.error(`new Error: ${err}`);
+  }
+})();
